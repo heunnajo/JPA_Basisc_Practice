d5e970d2e8bfecfc03a7ed3d4a00f4c1e373a237
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-16 14:20:25 +0900

    Added a value type Address.
    Added a common method fullAddress() and validation constraints using @Column(length).

--- src/main/java/jpashop/jpabook/domain/Member.java
+++ src/main/java/jpashop/jpabook/domain/Member.java
@@ -11,9 +11,10 @@ public class Member extends BaseEntity {
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded//값 탕입이라는 것을 명시해주기 위해
+    private Address address;
+
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
@@ -33,35 +34,19 @@ public class Member extends BaseEntity {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
     public List<Order> getOrders() {
         return orders;
     }
 
     public void setOrders(List<Order> orders) {
         this.orders = orders;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
